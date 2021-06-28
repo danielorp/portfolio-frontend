d6be89adfab7bf87673c3104a55d56e74263f1f0
Daniel Orpinelli <danielorp@hotmail.com>
2021-06-28 19:41:26 -0300

    making url reusable

--- src/Services/RequestService.js
+++ src/Services/RequestService.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
+const BASE_URL = 'http://127.0.0.1:8000/'
+
 const http = axios.create({
-    baseURL: 'http://127.0.0.1:8000/',
-    withCredentials: true
+    baseURL: BASE_URL,
+    withCredentials: true,
 });
 
 export default http;
